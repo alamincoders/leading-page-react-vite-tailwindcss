f3d9810beb204b5696a3b1bfffd0643d2650dcf2
alamincoders <developeralamin03@gmail.com>
2023-09-18 00:38:46 +0600

    updated about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Hero from "./components/screen/Hero";
 import Gallery from "./components/screen/Gallery";
+import About from "./components/screen/About";
 
 export default function App() {
   return (
     <React.Fragment>
       <Hero />
-      <Gallery/>
-      <section>3</section>
+      <Gallery />
+      <About />
       <section>4</section>
     </React.Fragment>
   );
