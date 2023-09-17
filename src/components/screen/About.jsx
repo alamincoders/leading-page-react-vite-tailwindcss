2472a443741d44cf155341e633626c13e418b275
alamincoders <developeralamin03@gmail.com>
2023-09-18 00:57:41 +0600

    updated

--- src/components/screen/About.jsx
+++ src/components/screen/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
                 </svg>{" "}
                 <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold font-poppins uppercase">About Us</h2>
               </div>
-              <p className="text-justify text-zinc-500/80 text-3xl ">
+              <p className="md:text-justify text-zinc-500/80 text-3xl ">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy
                 text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
                 survived not only five centuries, but also the leap into electronic typesetting, remaining
@@ -32,7 +32,7 @@ const About = () => {
               <img className="w-full h-full object-cover object-top" src={aboutImg} alt="" />
             </div>
             <div className="">
-              <p className="text-justify text-zinc-500/80 text-3xl ">
+              <p className="md:text-justify text-zinc-500/80 text-3xl ">
                 essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
                 recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It has survived not only five
                 centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
