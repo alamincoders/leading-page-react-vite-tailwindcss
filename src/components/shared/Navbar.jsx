5aeeb57a83fbef57eb42761b9919c2fad51203da
alamincoders <developeralamin03@gmail.com>
2023-09-18 11:43:17 +0600

    creating navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,5 +1,36 @@
+import logo from "../../assets/images/nav_logo.png";
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="text-white">
+      <div>
+        <ul>
+          <div>
+            <li className="">
+              <img src={logo} alt="" />
+            </li>
+          </div>
+          <div>
+            <li>
+              <span></span>
+              <span>Home</span>
+            </li>
+            <li>
+              <span></span>
+              <span>About</span>
+            </li>
+            <li>
+              <span></span>
+              <span>Contact</span>
+            </li>
+          </div>
+          <div>
+            <li></li>
+            <li></li>
+          </div>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
