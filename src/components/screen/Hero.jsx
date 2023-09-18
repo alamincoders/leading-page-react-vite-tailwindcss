afddec0e6fcb7422da05aff0b0b224fb52efc7f3
alamincoders <developeralamin03@gmail.com>
2023-09-18 22:47:56 +0600

    home hero updated

--- src/components/screen/Hero.jsx
+++ src/components/screen/Hero.jsx
@@ -3,6 +3,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import bg from "../../assets/images/17.jpg";
+import bg2 from "../../assets/images/21.jpg";
+import bg3 from "../../assets/images/23.jpg";
 
 const Hero = () => {
   const settings = {
@@ -10,6 +12,8 @@ const Hero = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
     slidesToScroll: 1,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
@@ -22,7 +26,7 @@ const Hero = () => {
             <div style={{ backgroundImage: `url(${bg})` }} className="bg-cover bg-top bg-no-repeat min-h-screen relative">
               <div className="text-center">
                 <div className=" ">
-                  <div className=" flex items-center justify-center h-screen">
+                  <div className=" flex items-center justify-center h-screen bg-black/30 text-white/60">
                     <div>
                       <h2 className="font-extrabold text-[45px] sm:text-[65px] md:text-[80px] lg:text-[118px] xl:text-[128px] leading-[1.25] ">
                         THE ARTIST
@@ -37,10 +41,10 @@ const Hero = () => {
             </div>
           </div>{" "}
           <div>
-            <div style={{ backgroundImage: `url(${bg})` }} className="bg-cover bg-top bg-no-repeat min-h-screen relative">
+            <div style={{ backgroundImage: `url(${bg2})` }} className="bg-cover bg-top bg-no-repeat min-h-screen relative">
               <div className="text-center">
                 <div className=" ">
-                  <div className=" flex items-center justify-center h-screen">
+                  <div className=" flex items-center justify-center h-screen bg-black/30 text-white/60">
                     <div>
                       <h2 className="font-extrabold text-[45px] sm:text-[65px] md:text-[80px] lg:text-[118px] xl:text-[128px] leading-[1.25] ">
                         THE ARTIST
@@ -55,10 +59,10 @@ const Hero = () => {
             </div>
           </div>{" "}
           <div>
-            <div style={{ backgroundImage: `url(${bg})` }} className="bg-cover bg-top bg-no-repeat min-h-screen relative">
+            <div style={{ backgroundImage: `url(${bg3})` }} className="bg-cover bg-top bg-no-repeat min-h-screen relative">
               <div className="text-center">
                 <div className=" ">
-                  <div className=" flex items-center justify-center h-screen">
+                  <div className=" flex items-center justify-center h-screen bg-black/30 text-white/60">
                     <div>
                       <h2 className="font-extrabold text-[45px] sm:text-[65px] md:text-[80px] lg:text-[118px] xl:text-[128px] leading-[1.25] ">
                         THE ARTIST
@@ -71,7 +75,7 @@ const Hero = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </div>{" "}
         </Slider>
       </div>
     </section>
@@ -82,12 +86,12 @@ export default Hero;
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
-  return <div className={className} style={{ ...style, display: "block", background: "red", marginRight: "100px" }} onClick={onClick} />;
+  return <div className={className} style={{ ...style, display: "block", background: "red", marginRight: "50px" }} onClick={onClick} />;
 }
 
 function SamplePrevArrow(props) {
   const { className, style, onClick } = props;
   return (
-    <div className={className} style={{ ...style, display: "block", background: "green", marginLeft: "100px", zIndex: "10" }} onClick={onClick} />
+    <div className={className} style={{ ...style, display: "block", background: "green", marginLeft: "50px", zIndex: "10" }} onClick={onClick} />
   );
 }
