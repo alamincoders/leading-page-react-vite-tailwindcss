271af3ef2ac0670eb81cba9333fa49f4f4f94318
alamincoders <developeralamin03@gmail.com>
2023-09-18 23:01:20 +0600

    gallery updated

---
+++ src/data/imagesGallery.js
@@ -0,0 +1,66 @@
+import img1 from "../assets/images/25.jpg";
+import img2 from "../assets/images/19.jpg";
+import img3 from "../assets/images/22.jpg";
+import img4 from "../assets/images/26.jpg";
+import img5 from "../assets/images/23.jpg";
+import img6 from "../assets/images/24.jpg";
+import img7 from "../assets/images/27.jpg";
+import img8 from "../assets/images/28.jpg";
+import img9 from "../assets/images/29.jpg";
+
+export const imagesGallery = [
+  {
+    id: 1,
+    ImageSrc: img1,
+    react: 18,
+    comment: 15,
+  },
+  {
+    id: 2,
+    ImageSrc: img2,
+    react: 154,
+    comment: 98,
+  },
+  {
+    id: 3,
+    ImageSrc: img3,
+    react: 147,
+    comment: 365,
+  },
+  {
+    id: 4,
+    ImageSrc: img4,
+    react: 24,
+    comment: 20,
+  },
+  {
+    id: 5,
+    ImageSrc: img5,
+    react: 45,
+    comment: 36,
+  },
+  {
+    id: 6,
+    ImageSrc: img6,
+    react: 22,
+    comment: 36,
+  },
+  {
+    id: 7,
+    ImageSrc: img7,
+    react: 10,
+    comment: 12,
+  },
+  {
+    id: 8,
+    ImageSrc: img8,
+    react: 788,
+    comment: 236,
+  },
+  {
+    id: 9,
+    ImageSrc: img9,
+    react: 598,
+    comment: 652,
+  },
+];
