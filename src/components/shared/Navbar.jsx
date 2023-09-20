4ce01f2d8d4f7aa72829213e5a84faa836a5ce56
alamincoders <developeralamin03@gmail.com>
2023-09-20 10:09:53 +0600

    updated

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../../assets/images/nav_logo.png";
+import logo from "../../assets/images/logo.png";
 import { useState } from "react";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
