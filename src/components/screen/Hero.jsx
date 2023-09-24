998d9c046cf97695c48db0bb70b17d30768727ec
alamincoders <developeralamin03@gmail.com>
2023-09-24 08:46:04 +0600

    updated hero section content

--- src/components/screen/Hero.jsx
+++ src/components/screen/Hero.jsx
@@ -40,54 +40,93 @@ const Hero = () => {
           <div>
             <div
               style={{ backgroundImage: `url(${bg})` }}
-              className="bg-cover bg-top bg-no-repeat h-[600px] sm:h-[650px] md:h-[700px] lg:h-[800px] xl:h-[1200px] relative"
+              className="bg-cover bg-top bg-no-repeat h-[700px] sm:h-[750px] md:h-[800px] lg:h-[1000px] xl:h-[1600px] relative"
             >
               <div className="text-center">
                 <div className=" ">
-                  <div className="flex items-center justify-center h-[600px] sm:h-[650px] md:h-[700px] lg:h-[800px] xl:h-[1200px] bg-black/40 text-white/60">
-                    <div className="mt-28 font-extrabold text-[40px] sm:text-[60px] md:text-[90px] lg:text-[120px] xl:text-[150px] leading-[1.25] font-tommy_bold">
-                      <h2 className="">THE ARTIST</h2>
-                      <h2 className="">CLOUD AGENCY</h2>
+                  <div className="flex items-center justify-center h-[700px] sm:h-[750px] md:h-[800px] lg:h-[1000px] xl:h-[1600px] bg-black/60 text-white">
+                    <div>
+                      <div className="mt-28 font-extrabold text-[40px] sm:text-[60px] md:text-[90px] lg:text-[120px] xl:text-[150px]  leading-[1.00]  font-tommy_bold">
+                        <h2 className="">THE ARTIST</h2>
+                        <h2 className="">CLOUD AGENCY</h2>
+                      </div>
+                      <div className="text-white/90 container_fluid font-sans ">
+                        <h3 className="uppercase text-[22px] sm:text-[28px] lg:text-[36px] xl:text-[42px] font-medium mt-3">
+                          where your ai identity finds its sanc tuary.
+                        </h3>
+                        <p className="text-base sm:text-[18px] lg:text-[22px] mt-6 sm:mt-12 lg:mt-[72px] font-medium leading-snug">
+                          In today&apos;s fast-paced digital landscape, protecting your unique AI identity is paramount. At The Artist Agency, we
+                          specialise in safeguarding the digital likeness of public figures, sports stars, entertainers, and content creators.
+                          We&apos;re here to empower you by creating, preserving, and protecting your AI identity, all while ensuring you retain full
+                          ownership and control
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>{" "}
+          </div>
           <div>
             <div
               style={{ backgroundImage: `url(${bg2})` }}
-              className="bg-cover bg-top bg-no-repeat h-[600px] sm:h-[650px] md:h-[700px] lg:h-[800px] xl:h-[1200px] relative"
+              className="bg-cover bg-top bg-no-repeat h-[700px] sm:h-[750px] md:h-[800px] lg:h-[1000px] xl:h-[1600px] relative"
             >
               <div className="text-center">
                 <div className=" ">
-                  <div className=" flex items-center justify-center h-[600px] sm:h-[650px] md:h-[700px] lg:h-[800px] xl:h-[1200px] bg-black/40 text-white/60">
-                    <div className="mt-28 font-extrabold text-[40px] sm:text-[60px] md:text-[90px] lg:text-[120px] xl:text-[150px] leading-[1.25] font-tommy_bold">
-                      <h2 className=" ">THE ARTIST</h2>
-                      <h2 className="">CLOUD AGENCY</h2>
+                  <div className="flex items-center justify-center h-[700px] sm:h-[750px] md:h-[800px] lg:h-[1000px] xl:h-[1600px] bg-black/60 text-white">
+                    <div>
+                      <div className="mt-28 font-extrabold text-[40px] sm:text-[60px] md:text-[90px] lg:text-[120px] xl:text-[150px]  leading-[1.00]  font-tommy_bold">
+                        <h2 className="">THE ARTIST</h2>
+                        <h2 className="">CLOUD AGENCY</h2>
+                      </div>
+                      <div className="text-white/90 container_fluid font-sans ">
+                        <h3 className="uppercase text-[22px] sm:text-[28px] lg:text-[36px] xl:text-[42px] font-medium mt-3">
+                          where your ai identity finds its sanc tuary.
+                        </h3>
+                        <p className="text-base sm:text-[18px] lg:text-[22px] mt-6 sm:mt-12 lg:mt-[72px] font-medium leading-snug">
+                          In today&apos;s fast-paced digital landscape, protecting your unique AI identity is paramount. At The Artist Agency, we
+                          specialise in safeguarding the digital likeness of public figures, sports stars, entertainers, and content creators.
+                          We&apos;re here to empower you by creating, preserving, and protecting your AI identity, all while ensuring you retain full
+                          ownership and control
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>{" "}
+          </div>
           <div>
             <div
               style={{ backgroundImage: `url(${bg3})` }}
-              className="bg-cover bg-top bg-no-repeat h-[600px] sm:h-[650px] md:h-[700px] lg:h-[800px] xl:h-[1200px] relative"
+              className="bg-cover bg-top bg-no-repeat h-[700px] sm:h-[750px] md:h-[800px] lg:h-[1000px] xl:h-[1600px] relative"
             >
               <div className="text-center">
                 <div className=" ">
-                  <div className=" flex items-center justify-center h-[600px] sm:h-[650px] md:h-[700px] lg:h-[800px] xl:h-[1200px] bg-black/40 text-white/60">
-                    <div className="mt-28 font-extrabold text-[40px] sm:text-[60px] md:text-[90px] lg:text-[120px] xl:text-[150px] leading-[1.25] font-tommy_bold">
-                      <h2 className=" ">THE ARTIST</h2>
-                      <h2 className="">CLOUD AGENCY</h2>
+                  <div className="flex items-center justify-center h-[700px] sm:h-[750px] md:h-[800px] lg:h-[1000px] xl:h-[1600px] bg-black/60 text-white">
+                    <div>
+                      <div className="mt-28 font-extrabold text-[40px] sm:text-[60px] md:text-[90px] lg:text-[120px] xl:text-[150px]  leading-[1.00]  font-tommy_bold">
+                        <h2 className="">THE ARTIST</h2>
+                        <h2 className="">CLOUD AGENCY</h2>
+                      </div>
+                      <div className="text-white/90 container_fluid font-sans ">
+                        <h3 className="uppercase text-[22px] sm:text-[28px] lg:text-[36px] xl:text-[42px] font-medium mt-3">
+                          where your ai identity finds its sanc tuary.
+                        </h3>
+                        <p className="text-base sm:text-[18px] lg:text-[22px] mt-6 sm:mt-12 lg:mt-[72px] font-medium leading-snug">
+                          In today&apos;s fast-paced digital landscape, protecting your unique AI identity is paramount. At The Artist Agency, we
+                          specialise in safeguarding the digital likeness of public figures, sports stars, entertainers, and content creators.
+                          We&apos;re here to empower you by creating, preserving, and protecting your AI identity, all while ensuring you retain full
+                          ownership and control
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>{" "}
+          </div>
         </Slider>
       </div>
     </section>
@@ -96,12 +135,11 @@ const Hero = () => {
 
 export default Hero;
 
-
 // next arrow icon here
 function SampleNextArrow(props) {
   const { className, onClick } = props;
   return (
-    <button className={`${className} !hidden w-0 h-0  mr-16   xl:mr-[18%] lg:!inline-block lg:w-20 lg:h-20`} onClick={onClick}>
+    <button className={`${className} !hidden w-0 h-0  mr-16    2xl:mr-[350px] lg:!inline-block lg:w-20 lg:h-20`} onClick={onClick}>
       <svg className="text-white inline-block" xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24">
         <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.3" d="m10 17l5-5l-5-5" />
       </svg>
@@ -112,7 +150,7 @@ function SampleNextArrow(props) {
 function SamplePrevArrow(props) {
   const { className, onClick } = props;
   return (
-    <button className={`${className} !hidden w-0 h-0 ml-5 xl:ml-[16%] lg:!inline-block lg:w-20 lg:h-20 z-10`} onClick={onClick}>
+    <button className={`${className} !hidden w-0 h-0 ml-5  2xl:ml-[300px] lg:!inline-block lg:w-20 lg:h-20 z-10`} onClick={onClick}>
       <svg className="text-white inline-block" xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24">
         <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.3" d="m14 7l-5 5l5 5" />
       </svg>
