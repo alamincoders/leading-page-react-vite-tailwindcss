b2bcf47b4fd281aa922c81f6449405476ed0c2e1
alamincoders <developeralamin03@gmail.com>
2023-09-24 12:09:17 +0600

    founder section updated

--- src/components/screen/Founder.jsx
+++ src/components/screen/Founder.jsx
@@ -1,95 +1,138 @@
 import founderImg from "../../assets/images/latest/img6.jpg";
+import founderImg2 from "../../assets/images/latest/img7.jpg";
 const Founder = () => {
   return (
-    <section id="founder" className="py-24 md:py-44 lg:py-72 relative overflow-hidden">
-      <div className="container_fluid">
-        <div className="w-full h-32 bg-black absolute z-0 hidden lg:block"></div>
-        <div className="grid grid-cols-1 xl:grid-cols-3 gap-8 md:gap-16 z-10 relative">
-          <div className="w-full h-full">
-            <img className="w-full h-full object-cover object-center" src={founderImg} alt="" />
-          </div>
-          <div className="xl:col-span-2 mt-5">
-            <div className="text-white/90 mb-12">
-              <h2 className=" text-3xl md:text-5xl lg:text-[50px] font-bold  uppercase font-tommy_bold leading-none">DAMON DANIELI</h2>
-              <h4 className="font-sans font-medium text-[22px] md:text-[28px] lg:text-[32px] leading-none">Co-Founder & CTO</h4>
+    <section>
+      <section id="founder" className="py-24 md:py-44 lg:py-72 relative overflow-hidden">
+        <div className="container_fluid">
+          <div className="w-full h-32 bg-black absolute z-0 hidden lg:block"></div>
+          <div className="grid grid-cols-1 xl:grid-cols-3 gap-8 md:gap-16 z-10 relative">
+            <div className="w-3/4 xl:w-full h-full">
+              <img className="w-full h-full object-cover object-center" src={founderImg} alt="" />
             </div>
-            <div>
-              <div className="text-base sm:text-[18px] lg:text-[22px] text-zinc-400 font-tommy">
-                <p className="my-5 leading-tight md:text-justify">
-                  Damon Danieli is a tech visionary with a remarkable track record in shaping the digital landscape. With a career spanning
-                  high-profile roles in the tech industry, Damon brings a wealth of experience to The Artist Agency.
-                </p>
+            <div className="xl:col-span-2 mt-5">
+              <div className="lg:text-white/90 mb-6 xl:mb-12">
+                <h2 className=" text-3xl md:text-5xl lg:text-[50px] font-bold  uppercase font-tommy_bold leading-none">DAMON DANIELI</h2>
+                <h4 className="font-sans font-medium text-[22px] md:text-[28px] lg:text-[32px] leading-none">Co-Founder & CTO</h4>
+              </div>
+              <div>
+                <div className="text-base sm:text-[18px] lg:text-[22px] text-zinc-400 font-tommy">
+                  <p className="my-5 leading-tight md:text-justify">
+                    Damon Danieli is a tech visionary with a remarkable track record in shaping the digital landscape. With a career spanning
+                    high-profile roles in the tech industry, Damon brings a wealth of experience to The Artist Agency.
+                  </p>
+                </div>
+              </div>
+              <div className="  text-zinc-400 font-tommy mt-5">
+                <h5 className="text-[17px] sm:text-[19px] lg:text-[23px] uppercase leading-none font-[500] font-sans text-zinc-500/90 mb-3">
+                  <strong>FORMER VICE PRESIDENT AT DOCUSIGN</strong>
+                </h5>
+                <div className="text-base sm:text-[18px] lg:text-[22px]">
+                  <p className="leading-tight  md:text-justify">
+                    During his tenure as Vice President of Manage within the DocuSign Agreement Cloud, Damon Danieli played a pivotal role in
+                    supporting the Machine Learning/Artificial Intelligence strategy. He took ownership of customer-facing features like In Product
+                    Reporting and security reporting through DocuSign Monitor. Additionally, he laid the foundations of DocuSign&apos;s ML/AI
+                    infrastructure.
+                  </p>
+                </div>
               </div>
             </div>
-            <div className="  text-zinc-400 font-tommy mt-5">
+          </div>
+          <div className="mt-12">
+            {/* =========================================== */}
+            <div className="  text-zinc-400 font-tommy mt-7">
               <h5 className="text-[17px] sm:text-[19px] lg:text-[23px] uppercase leading-none font-[500] font-sans text-zinc-500/90 mb-3">
-                <strong>FORMER VICE PRESIDENT AT DOCUSIGN</strong>
+                <strong>FOUNDER AND CEO/CTO OF APPURI (ACQUIRED BY DOCUSIGN)</strong>
               </h5>
               <div className="text-base sm:text-[18px] lg:text-[22px]">
                 <p className="leading-tight  md:text-justify">
-                  During his tenure as Vice President of Manage within the DocuSign Agreement Cloud, Damon Danieli played a pivotal role in supporting
-                  the Machine Learning/Artificial Intelligence strategy. He took ownership of customer-facing features like In Product Reporting and
-                  security reporting through DocuSign Monitor. Additionally, he laid the foundations of DocuSign&apos;s ML/AI infrastructure.
+                  Damon&apos;s entrepreneurial spirit shone through as he founded Appuri, a groundbreaking Customer Data Platform. Appuri&apos;s
+                  acquisition by DocuSign in December 2017 underscored its significance in the SaaS industry. The platform empowered businesses to
+                  optimize retention, engagement, and revenue by capturing and connecting behavioural data from various sources.
+                </p>
+              </div>
+            </div>
+            {/* ================================================ */}
+            <div className="  text-zinc-400 font-tommy mt-7">
+              <h5 className="text-[17px] sm:text-[19px] lg:text-[23px] uppercase leading-none font-[500] font-sans text-zinc-500/90 mb-3">
+                <strong>FOUNDER AND CTO OF Z2LIVE (ACQUIRED BY KING)</strong>
+              </h5>
+              <div className="text-base sm:text-[18px] lg:text-[22px]">
+                <p className="leading-tight  md:text-justify">
+                  Before Appuri, Damon&apos;s journey included leading Z2Live, a prominent developer of free-to-play social games on iOS. His
+                  contributions led to over 50 million downloads of titles such as Battle Nations, Trade Nations, and MetalStorm: Wingman.
+                  Z2Live&apos;s acquisition by King in February 2015 marked a testament to Damon&apos;s impact on the gaming world.
+                </p>
+              </div>
+            </div>
+            {/* =========================================== */}
+            <div className="  text-zinc-400 font-tommy mt-7">
+              <h5 className="text-[17px] sm:text-[19px] lg:text-[23px] uppercase leading-none font-[500] font-sans text-zinc-500/90 mb-3">
+                <strong>FOUNDER AND CEO/CTO OF APPURI (ACQUIRED BY DOCUSIGN)</strong>
+              </h5>
+              <div className="text-base sm:text-[18px] lg:text-[22px]">
+                <p className="leading-tight  md:text-justify">
+                  Damon&apos;s entrepreneurial spirit shone through as he founded Appuri, a groundbreaking Customer Data Platform. Appuri&apos;s
+                  acquisition by DocuSign in December 2017 underscored its significance in the SaaS industry. The platform empowered businesses to
+                  optimize retention, engagement, and revenue by capturing and connecting behavioural data from various sources.
+                </p>
+              </div>
+            </div>
+            {/* ================================================ */}
+            <div className="  text-zinc-400 font-tommy mt-7">
+              <h5 className="text-[17px] sm:text-[19px] lg:text-[23px] uppercase leading-none font-[500] font-sans text-zinc-500/90 mb-3">
+                <strong>MICROSOFT VETERAN</strong>
+              </h5>
+              <div className="text-base sm:text-[18px] lg:text-[22px]">
+                <p className="leading-tight  md:text-justify">
+                  Damon&apos;s illustrious career also includes 14 years at Microsoft, where he played a pivotal role in designing the original Xbox
+                  Live. He crafted functional specifications for marquee features like voice chat, voice commands, and friends. His leadership
+                  extended to managing complex technical challenges and teams of project managers. Gamers may recognize him by his Xbox Live Gamertag.
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <div className="mt-12">
-          {/* =========================================== */}
-          <div className="  text-zinc-400 font-tommy mt-7">
-            <h5 className="text-[17px] sm:text-[19px] lg:text-[23px] uppercase leading-none font-[500] font-sans text-zinc-500/90 mb-3">
-              <strong>FOUNDER AND CEO/CTO OF APPURI (ACQUIRED BY DOCUSIGN)</strong>
-            </h5>
-            <div className="text-base sm:text-[18px] lg:text-[22px]">
-              <p className="leading-tight  md:text-justify">
-                Damon&apos;s entrepreneurial spirit shone through as he founded Appuri, a groundbreaking Customer Data Platform. Appuri&apos;s
-                acquisition by DocuSign in December 2017 underscored its significance in the SaaS industry. The platform empowered businesses to
-                optimize retention, engagement, and revenue by capturing and connecting behavioural data from various sources.
-              </p>
+      </section>
+
+      {/* ====================== */}
+      <section id="founder" className="pb-24 md:pb-44 lg:pb-72 relative overflow-hidden">
+        <div className="container_fluid">
+          <div className="w-full h-32 bg-black absolute z-0 hidden lg:block xl:right-[30%] 2xl:right-[35%]"></div>
+
+          <div className="grid grid-cols-1 xl:grid-cols-3 gap-8 md:gap-16 z-10 relative">
+            <div className="xl:col-span-2 mt-5 order-2 xl:order-1">
+              <div className="lg:text-white/90 mb-6 xl:mb-12">
+                <h2 className=" text-3xl md:text-5xl lg:text-[50px] font-bold  uppercase font-tommy_bold leading-none">SISI MAKHANYA DANIELI</h2>
+                <h4 className="font-sans font-medium text-[22px] md:text-[28px] lg:text-[32px] leading-none">Co-Founder & CEO</h4>
+              </div>
+              <div>
+                <div className="text-base sm:text-[18px] lg:text-[22px] text-zinc-400 font-tommy">
+                  <p className="my-5 leading-tight md:text-justify">
+                    Sisi Mak Danieli, is a South African beauty queen, and entrepreneur with a strong foothold in both the entertainment and business
+                    realms. Her journey is a testament to her passion for both fields
+                  </p>
+                </div>
+              </div>
+              <div className="  text-zinc-400 font-tommy mt-5">
+                <h5 className="text-[17px] sm:text-[19px] lg:text-[23px] uppercase leading-none font-[500] font-sans text-zinc-500/90 mb-3">
+                  <strong>FORMER CEO OF MTPROJECTS</strong>
+                </h5>
+                <div className="text-base sm:text-[18px] lg:text-[22px]">
+                  <p className="leading-tight  md:text-justify">
+                    Sisi held the position of CEO at MTProjects, a civil works construction company, for 12 years. Her responsibilities included
+                    overseeing the operations of this multi-million Rand SMME business while actively marketing it.
+                  </p>
+                </div>
+              </div>
+              <div className="mb-10"></div>
             </div>
-          </div>
-          {/* ================================================ */}
-          <div className="  text-zinc-400 font-tommy mt-7">
-            <h5 className="text-[17px] sm:text-[19px] lg:text-[23px] uppercase leading-none font-[500] font-sans text-zinc-500/90 mb-3">
-              <strong>FOUNDER AND CTO OF Z2LIVE (ACQUIRED BY KING)</strong>
-            </h5>
-            <div className="text-base sm:text-[18px] lg:text-[22px]">
-              <p className="leading-tight  md:text-justify">
-                Before Appuri, Damon&apos;s journey included leading Z2Live, a prominent developer of free-to-play social games on iOS. His
-                contributions led to over 50 million downloads of titles such as Battle Nations, Trade Nations, and MetalStorm: Wingman. Z2Live&apos;s
-                acquisition by King in February 2015 marked a testament to Damon&apos;s impact on the gaming world.
-              </p>
-            </div>
-          </div>
-          {/* =========================================== */}
-          <div className="  text-zinc-400 font-tommy mt-7">
-            <h5 className="text-[17px] sm:text-[19px] lg:text-[23px] uppercase leading-none font-[500] font-sans text-zinc-500/90 mb-3">
-              <strong>FOUNDER AND CEO/CTO OF APPURI (ACQUIRED BY DOCUSIGN)</strong>
-            </h5>
-            <div className="text-base sm:text-[18px] lg:text-[22px]">
-              <p className="leading-tight  md:text-justify">
-                Damon&apos;s entrepreneurial spirit shone through as he founded Appuri, a groundbreaking Customer Data Platform. Appuri&apos;s
-                acquisition by DocuSign in December 2017 underscored its significance in the SaaS industry. The platform empowered businesses to
-                optimize retention, engagement, and revenue by capturing and connecting behavioural data from various sources.
-              </p>
-            </div>
-          </div>
-          {/* ================================================ */}
-          <div className="  text-zinc-400 font-tommy mt-7">
-            <h5 className="text-[17px] sm:text-[19px] lg:text-[23px] uppercase leading-none font-[500] font-sans text-zinc-500/90 mb-3">
-              <strong>MICROSOFT VETERAN</strong>
-            </h5>
-            <div className="text-base sm:text-[18px] lg:text-[22px]">
-              <p className="leading-tight  md:text-justify">
-                Damon&apos;s illustrious career also includes 14 years at Microsoft, where he played a pivotal role in designing the original Xbox
-                Live. He crafted functional specifications for marquee features like voice chat, voice commands, and friends. His leadership extended
-                to managing complex technical challenges and teams of project managers. Gamers may recognize him by his Xbox Live Gamertag.
-              </p>
+            <div className="w-3/4  xl:w-full h-full -mt-1 order-1 xl:order-2">
+              <img className="w-full h-full object-cover object-center" src={founderImg2} alt="" />
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </section>
   );
 };
